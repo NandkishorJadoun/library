e5e440eab28f79eac24572d15717ec0e527a84d2
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-02-05 15:24:39 +0530

    add logic to add books on display

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
-const myLibrary = [];
+let myLibrary = [];
+
+const bookContainer = document.querySelector(".book-container")
 
 function Book(title, author, pages, read) {
-  // the constructor...
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  // take params, create a book then store it in the array
+    // take params, create a book then store it in the array
+    const book = new Book(title, author, pages, read);
 
-  const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
 
-  myLibrary.push(book)
+
+    function displayBook(array) {
+        bookContainer.innerHTML = "";
+        array.forEach((item) => {
+            const book = document.createElement("div")
+            book.setAttribute("class", "book")
+            bookContainer.appendChild(book)
+
+            for (const key in item) {
+                const info = document.createElement("p")
+                book.appendChild(info)
+
+                info.textContent = item[key]
+            }
+        });
+    }
+    displayBook(myLibrary)
 }
 
-addBookToLibrary("deep work", "cal", 200, "yes")
 
-console.log(myLibrary)
+const dialogBox = document.querySelector("dialog")
+const dialogOpener = document.querySelector(".dialog-opener")
+const submitBtn = document.querySelector("#submit-button")
+
+const bookInp = document.querySelector("#book-input")
+const authorInp = document.querySelector("#author-input")
+const pageInp = document.querySelector("#no-of-pages")
+const readInp = document.querySelector("#read-input")
 
 
+dialogOpener.addEventListener("click", () => {
+    dialogBox.showModal()
+})
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (!bookInp.value || !authorInp.value || !pageInp.value) {
+        alert("Please fill out!");
+        return;
+    }
+
+    readInp.value = readInp.checked ? "Yes" : "No"
+
+    const bookInfo = bookInp.value;
+    const authorInfo = authorInp.value;
+    const pageInfo = pageInp.value;
+    const readInfo = readInp.value;
+
+    bookInp.value = "";
+    authorInp.value = "";
+    pageInp.value = "";
+    
+    addBookToLibrary(bookInfo, authorInfo, pageInfo, readInfo)
+
+    dialogBox.close();
+})
